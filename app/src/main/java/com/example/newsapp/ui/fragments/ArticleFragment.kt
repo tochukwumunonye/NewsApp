4676ac44175714ba4a7351e4e431765cd3ed1994
Tochukwumunonye <tochukwuokeymunonye@gmail.com>
2020-09-20 17:27:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,35 @@
+package com.example.newsapp.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.newsapp.R
+import com.example.newsapp.ui.NewsActivity
+import com.example.newsapp.ui.viewmodels.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_article.*
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+    lateinit var viewModel : NewsViewModel
+
+    val args: ArticleFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+
+        val article = args.article
+        webView.apply{
+            webViewClient = WebViewClient()
+            article.url?.let { loadUrl(it) }
+        }
+
+        fab.setOnClickListener {
+            viewModel.addArticle(article)
+            Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/ui/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.ui.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "364a083c17e14c3d9318a30ccec4ef9f"
+        const val BASE_URL = "https://newsapi.org"
+        const val  SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
